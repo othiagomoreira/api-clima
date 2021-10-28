6db31522dfdeec389252d4229814f6989cd447bd
Thiago Almeida <contatothiagomoreira@outlook.com>
2021-10-28 15:47:58 -0300

    projeto finalizado

--- script.js
+++ script.js
@@ -4,14 +4,17 @@ document.querySelector('.form').addEventListener('submit', async (event) => {
     event.preventDefault(); //previne o evento padrão
 
     // Pega oque foi digitado no input
-    const input = document.querySelector('#searchInput').value;
+    const input = document.querySelector('#searchInput');
 
     // Algo foi digitado ou não
-    if(input !== ''){
+    if(input.value !== ''){
+        clearInfo(); // Limpa
+
+        input.style.border = 'none'; //Retira a border que é ativada quando o input está vazio
         showWarning('Loading...')
 
         //URL da API
-        const url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(input)}&appid=393134eedb6c188d1460ca78f22ca9b1&units=metric&lang=pt_br`
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(input.value)}&appid=393134eedb6c188d1460ca78f22ca9b1&units=metric&lang=pt_br`
        
         const results = await fetch(url);
         const json = await results.json();
@@ -31,30 +34,37 @@ document.querySelector('.form').addEventListener('submit', async (event) => {
             });
 
         } else {
+            clearInfo();
             showWarning(json.message); /*A API fornece uma propriedade com uma mensagem caso a city não seja encrontrada */
         }
 
     } else {
-
+        clearInfo();
+        
     }
 });
 
 // Responsavel por mostrar o nome objeto com as infomarções da API
 function showInfo(json) {
     showWarning(''); // Seta como vazio para que não exiba mais a mensagem de loading
-
-    // Exibi o conteudo do objeto na tela
-    document.querySelector('.content').style.display = 'block';
-
+    
     document.querySelector('.content__localization').innerText = `${json.name}, ${json.country}`;
     document.querySelector('.temp__graus').innerHTML = `${json.temp} <sup>ºC</sup>`;
     document.querySelector('.wind__info').innerHTML = `${json.windSpedd} <span>Km/h</span>`;
-
+    
     // Exibi icones diferentes conforme o tempo, para isso é preciso pegar a url fornecida pela API e trocar somente a parte onde fica o icone por nossa proprieda tempIcon
-    document.querySelector('.content__info img').setAttribute('src', `http://openweathermap.org/img/wn/${json.tempIncon}@2x.png`)
-
+    document.querySelector('.content__temp img').setAttribute('src', `http://openweathermap.org/img/wn/${json.tempIncon}@2x.png`);
+    
     // Ponteiro dinamico que muda sua posiçao conforme a velocidade do vento(windSpeed), é preciso diminuir 90 graus para compensar e ficar na posição correta
-    document.querySelector('.wind__pointer').style.transform = `rotate(${json.windAngle - 90}deg)` 
+    document.querySelector('.wind__pointer').style.transform = `rotate(${json.windAngle - 90}deg)`;
+
+    // Exibi todos na tela
+    document.querySelector('.content').style.display = 'block';
+}
+
+function clearInfo() {
+    showWarning('');
+    document.querySelector('.content').style.display = 'none';
 }
 
 // Mostrar ou remover avisos
